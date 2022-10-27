7d8f54663602f60fc4c73d973dd75b9be84d07a7
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-27 18:54:52 +0800

    Create arithmetic functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return 'error';
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    let result;
+    switch (operator) {
+        case '+': 
+            result = add(a, b);
+            break;
+        case '-': 
+            result = subtract(a, b);
+            break;
+        case '*': 
+            result = multiply(a, b);
+            break;
+        case '/': 
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
