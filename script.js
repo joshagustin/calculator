9de19aa4050a7d141884d69966a0f533598fb356
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-31 21:41:43 +0800

    Create evaluateEquation

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clear = document.querySelector('.clear');
 const negate = document.querySelector('.negate');
 
 numbers.forEach(number => number.addEventListener('click', numberHandler));
+operators.forEach(operator => operator.addEventListener('click', operatorHandler));
 negate.addEventListener('click', negateNumber);
 clear.addEventListener('click', clearAll);
 
@@ -20,8 +21,37 @@ function numberHandler() {
     updateDisplay(displayValue + this.textContent);
 }
 
+function operatorHandler() {
+    const operatorType = this.classList[1];
+    const operatorSymbol = this.textContent;
+    // change operator
+    if (equation.operator && !operandExists('second')) {
+        displayValue = displayValue.slice(0, -1);
+    }
+    // successive operations
+    else if (equation.operator && operandExists('second')) {
+        evaluateEquation();
+    }
+    else if (equation.operator) {
+        generateSyntaxError();
+        return;
+    }
+    equation.operator = operatorType;
+    updateDisplay(displayValue + operatorSymbol);
+}
+
+function evaluateEquation() {
+    const arr = displayValue.split(EQUATION_DELIMITERS);
+    equation.aOperand = Number(arr[0]);
+    equation.bOperand = Number(arr[1]);
+    const result = operate(equation.operator, equation.aOperand, equation.bOperand);
+    clearEquation();
+    equation.aOperand = result;
+    updateDisplay(result.toString());
+}
+
 function negateNumber() {
-    const equationOperator = displayValue.split(/[0-9.]+/);
+    const operatorArr = displayValue.split(/[0-9.-]+/);
     const arr = displayValue.split(EQUATION_DELIMITERS);
     const arrLen = arr.length;
     if (arrLen === 2) {
@@ -34,7 +64,7 @@ function negateNumber() {
     }
 
     displayValue = arrLen === 2 ?
-        arr[0] + equationOperator + arr[1] : arr[0];
+        arr[0] + operatorArr[1] + arr[1] : arr[0];
     updateDisplay (displayValue)
 }
 
@@ -42,6 +72,15 @@ function updateDisplay(str) {
     displayValue = str;
     const display = document.querySelector('.display');
     display.textContent = displayValue;
+}
+
+function operandExists(option) {
+    const arr = displayValue.split(EQUATION_DELIMITERS);
+    if (option === 'first') {
+        return arr[0].length;
+    }
+    if (arr.length < 2) return false;
+    return arr[1].length > 0;
 }
 
 function clearAll() {
