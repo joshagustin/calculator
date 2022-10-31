eb650fe3d813c73a4edbd7f4d1661ee4827cebe4
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-31 13:42:01 +0800

    Refactor operatorInputHandler

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ operators.forEach(operator => operator.addEventListener('click', operatorInputHa
 equals.addEventListener('click', equalsInputHandler);
 clear.addEventListener('click', clearAll);
 
-const EQUATION_DELIMITERS = /[+x\/\s-]+/;
+const EQUATION_DELIMITERS = /[+x\/-]+/;
 let displayValue = '';
 const equation = {
     operator: null,
@@ -58,37 +58,35 @@ function equalsInputHandler() {
 }
 
 function operatorInputHandler() {
-     // change operator
-     if (equation.operator && !operandExists(2)) {
-        equation.operator = this.classList[1];
-        updateDisplay(displayValue.replace(EQUATION_DELIMITERS, this.textContent));
+    const operatorType = this.classList[1];
+    const operatorSymbol = this.textContent;
+    // set second operand to negative
+    if (equation.operator && !operatorIsValid() && operatorType === 'sub') {
+        operatorType = equation.operator;
+    }
+    // change operator 
+    else if (equation.operator && !operandExists(2)) {
+        displayValue = displayValue.slice(0, -1);
     }
     // successive operations involving negative first operand
     else if (equation.aOperand < 0 && equation.operator && operandExists(2)) {
         setAndEvaluateEquation(this);
     }
-    // add operator to equation with negative first operand
-    else if (equation.aOperand < 0) {
-        equation.operator = this.classList[1];
-        updateDisplay(displayValue + this.textContent)
-    } 
-    // sign is attached to first operand
+    // attach sign to first operand
     else if (equation.operator && operatorIsValid() && !operandExists(1)){
         clearEquation();
-        equation.aOperand = Number(displayValue.replaceAll(' ', ''));
-        equation.operator = this.classList[1];
-        updateDisplay(equation.aOperand.toString() + this.textContent);
+        equation.aOperand = Number(displayValue);
     } 
     // successive operations
     else if (operandExists(1) && operandExists(2)) {
         setAndEvaluateEquation(this);
     }
     // add operator to equation
-    else {
+    else if (!equation.aOperand) {
         equation.aOperand = Number(displayValue.split(EQUATION_DELIMITERS)[0]);
-        equation.operator = this.classList[1];
-        updateDisplay(displayValue + this.textContent);
     }
+    equation.operator = operatorType;
+    updateDisplay(displayValue + operatorSymbol);
 }
 
 function setAndEvaluateEquation(node) {
@@ -100,15 +98,8 @@ function setAndEvaluateEquation(node) {
     }
     const result = operate(equation.operator, equation.aOperand, equation.bOperand);
     equation.aOperand = result;
-    // successive operations
-    if (node.classList[0] === 'operator') {
-        equation.operator = node.classList[1];
-        updateDisplay(result.toString() + node.textContent);
-    }
-    else {
-        equation.operator = null;
-        updateDisplay(result.toString());
-    } 
+    equation.operator = null;
+    updateDisplay(result.toString());
 }
 
 // determines result based on displayValue 
